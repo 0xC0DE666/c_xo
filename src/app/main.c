1fa76cfdceeb73e8c8b1d7b49f3428899426e8df
PatMan10 <PatMan10@protonmail.com>
2024-09-02 19:03:03 +0200

    update: init game logic

--- src/app/main.c
+++ src/app/main.c
@@ -12,10 +12,27 @@
 int main() {
   printf("C XO\n");
 
-  printf("%d %d\n", true, false);
+  Player* p1 = player_new("Player 1", 'X');
+  Player* p2 = player_new("Player 2", 'O');
+  Matrix* board = board_new(3, 3);
 
-  Position p = position_new(100, 12);
-  printf("%d %d\n", p.row, p.column);
+  bool win = false;
+  bool board_has_capacity = true;
+
+  while (board_has_capacity && win == false) {
+    char* str_p1 = player_to_string(p1);
+    char* str_p2 = player_to_string(p2);
+    char* str_board = matrix_to_string(board, (ToStringFn) square_to_string);
+
+    printf("%s  vs  %s\n%s", str_p1, str_p2, str_board);
+
+    free(str_p1);
+    free(str_p2);
+    free(str_board);
+
+    win = true;
+  }
+
 
   return 0;
 }
